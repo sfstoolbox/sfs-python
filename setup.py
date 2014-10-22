b05fa128da53a882e78d9a02773fe7836c6e6ee0
Matthias Geier <Matthias.Geier@gmail.com>
2014-10-22 14:49:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import sfs
+
+setup(
+    name="SoundFieldSynthesis",
+    version=sfs.__version__,
+    packages=['sfs'],
+
+    author="SFS Toolbox Developers",
+    author_email="sfstoolbox@gmail.com",
+    description="Sound Field Synthesis Toolbox",
+    long_description=open('README.rst').read(),
+    license="MIT",
+    keywords="audio SFS WFS Ambisonics".split(),
+    url="http://github.com/sfstoolbox",
+    download_url="http://github.com/sfstoolbox/sfs-python/releases",
+    platforms='any',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+)
