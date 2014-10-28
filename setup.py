2b7f15ca8b704833951d1daf30555dfc2d7ed397
Matthias Geier <Matthias.Geier@gmail.com>
2014-10-28 12:28:54 +0100

    Integrate tests with setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+# See http://pytest.org/latest/goodpractises.html
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 exec(open('sfs/_version.py').read())
 
@@ -23,4 +46,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
     ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
