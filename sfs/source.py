e1789552bb4cdb1417a0ec6fe3f6762f73739801
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-10-30 15:50:30 +0100

    Added inner product again

--- sfs/source.py
+++ sfs/source.py
@@ -16,9 +16,8 @@ def point(k, x0, x, y, z):
 
 def line(k, x0, x, y, z):
     """Line source parallel to the z-axis"""
-    #              j   (2)
-    # G(x-xs,w) =  -  H0  ( k |x-x0| )
-    #         
+    #                 (2)
+    # G(x-xs,w) =  j H0  ( k |x-x0| )
     xx, yy, zz = np.meshgrid(x - x0[0], y - x0[1], z, sparse=True)
     r = np.sqrt((xx) ** 2 + (yy) ** 2)
     return np.squeeze(special.hankel2(0, k * r))
@@ -29,4 +28,4 @@ def plane(k, n0, x, y, z):
     # G(x,w) = e^(-i w/c n x)
     xx, yy, zz = np.meshgrid(x, y, z, sparse=True)
     n0 = np.asarray(n0)
-    return np.squeeze(np.exp(-1j * k * ( n0[0] * xx + n0[1] * yy + n0[2] * zz))) 
+    return np.squeeze(np.exp(-1j * k * np.inner(np.array([xx, yy, zz]), n0)))
