b9f1cc3db43674b4f6e8260469e66a87f93ed102
Matthias Geier <Matthias.Geier@gmail.com>
2014-11-04 15:14:46 +0100

    Hide internal driving functions

--- sfs/mono/drivingfunction.py
+++ sfs/mono/drivingfunction.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.core.umath_tests import inner1d  # element-wise inner product
 
 
-def wfs_ps(k, x0, n0, xs):
+def _wfs_ps(k, x0, n0, xs):
     """Point source by two-dimensional WFS."""
     #                (x0-xs) n0
     # D(x0,k) = j k ------------- e^(-j k |x0-xs|)
@@ -17,7 +17,7 @@ def wfs_ps(k, x0, n0, xs):
 
 def wfs_2d_ps(k, x0, n0, xs):
     """Point source by two-dimensional WFS."""
-    return wfs_ps(k, x0, n0, xs)
+    return _wfs_ps(k, x0, n0, xs)
 
 
 def wfs_25d_ps(k, x0, n0, xs, xref=[0, 0, 0]):
@@ -35,10 +35,10 @@ def wfs_25d_ps(k, x0, n0, xs, xref=[0, 0, 0]):
 
 def wfs_3d_ps(k, x0, nx0, xs):
     """Point source by three-dimensional WFS."""
-    return wfs_ps(k, x0, nx0, xs)
+    return _wfs_ps(k, x0, nx0, xs)
 
 
-def wfs_pw(k, x0, n0, n=[0, 1, 0]):
+def _wfs_pw(k, x0, n0, n=[0, 1, 0]):
     """Plane wave by WFS."""
     # D(x0,k) =  j k n n0  e^(-j k n x0)
     x0 = np.asarray(x0)
@@ -49,7 +49,7 @@ def wfs_pw(k, x0, n0, n=[0, 1, 0]):
 
 def wfs_2d_pw(k, x0, n0, n=[0, 1, 0]):
     """Plane wave by two-dimensional WFS."""
-    return wfs_pw(k, x0, n0, n)
+    return _wfs_pw(k, x0, n0, n)
 
 
 def wfs_25d_pw(k, x0, n0, n=[0, 1, 0], xref=[0, 0, 0]):
@@ -66,7 +66,7 @@ def wfs_25d_pw(k, x0, n0, n=[0, 1, 0], xref=[0, 0, 0]):
 def wfs_3d_pw(k, x0, n0, n=[0, 1, 0]):
     """Plane wave by three-dimensional WFS.
        Eq.(17) from [Spors et al, 2008]"""
-    return wfs_pw(k, x0, n0, n)
+    return _wfs_pw(k, x0, n0, n)
 
 
 def delay_3d_pw(k, x0, n0, n=[0, 1, 0]):
@@ -82,7 +82,7 @@ def source_selection_pw(n0, n):
     n0 = np.asarray(n0)
     n = np.asarray(n)
     return np.inner(n, n0) >= 0
-    
+
 def source_selection_ps(n0, x0, xs):
     """Secondary source selection for a point source.
        Eq.(15) from [Spors et al, 2008]"""
