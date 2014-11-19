53cce08fb46742be921a2313a3fa0f4538fda18e
Sascha Spors <Sascha.Spors@uni-rostock.de>
2014-11-19 17:05:00 +0100

    Optional rotation of rectangular array

--- sfs/array.py
+++ sfs/array.py
@@ -4,19 +4,15 @@ import numpy as np
 from . import util
 
 
-def linear(N, dx, center=[0, 0, 0], n0=None):
+def linear(N, dx, center=[0, 0, 0], n0=[1, 0, 0]):
     """Linear secondary source distribution."""
     center = np.squeeze(np.asarray(center, dtype=np.float64))
     positions = np.zeros((N, 3))
     positions[:, 1] = (np.arange(N) - N / 2 + 1 / 2) * dx
-    if n0 is None:
-        n0 = np.array([1, 0, 0], dtype=np.float64)
-    else:
-        n0 = np.array(n0, dtype=np.float64)
-        R = util.rotation_matrix([1, 0, 0], n0)
-        positions = np.inner(positions, R)
+    directions = np.tile([1, 0, 0], (N, 1))
+    positions, directions = _rotate_array(positions, directions, [1, 0, 0], n0)
     positions += center
-    directions = np.tile(n0, (N, 1))
+
     return positions, directions
 
 
@@ -33,29 +29,43 @@ def circular(N, R, center=[0, 0, 0]):
     return positions, directions
 
 
-def rectangular(Nx, dx, Ny, dy, center=[0, 0, 0]):
+def rectangular(Nx, dx, Ny, dy, center=[0, 0, 0], n0=None):
     """Rectangular secondary source distribution."""
 
     # left array
-    x00, n00 = linear(Nx, dx)
+    x00, n00 = linear(Ny, dy)
     positions = x00
     directions = n00
     # upper array
-    x00, n00 = linear(Ny, dy, center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
+    x00, n00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
                       n0=[0, -1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
     # right array
-    x00, n00 = linear(Nx, dx, center=[x00[-1, 0] + dx/2, 0, 0], n0=[-1, 0, 0])
+    x00, n00 = linear(Ny, dy, center=[x00[-1, 0] + dx/2, 0, 0], n0=[-1, 0, 0])
     x00 = np.flipud(x00)
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
     # lower array
-    x00, n00 = linear(Ny, dy, center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
+    x00, n00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
                       n0=[0, 1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
-
-    positions += np.asarray(center) - np.asarray([Nx/2 * dx, 0, 0])
+    # shift array to center
+    positions -= np.asarray([Nx/2 * dx, 0, 0])
+    # rotate array
+    if n0 is not None:
+        positions, directions = _rotate_array(positions, directions,
+                                              [1, 0, 0], n0)
+    # shift array to desired positions
+    positions += np.asarray(center)
 
     return positions, directions
+
+
+def _rotate_array(x0, n0, n1, n2):
+    """Rotate secondary sources from n1 to n2."""
+    R = util.rotation_matrix(n1, n2)
+    x0 = np.inner(x0, R)
+    n0 = np.inner(n0, R)
+    return x0, n0
