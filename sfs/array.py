f616aed21dd39b03183aec875cc2a224fb955111
Sascha Spors <Sascha.Spors@uni-rostock.de>
2014-12-02 15:46:10 +0100

    Secondary source weights, non-uniform linear array, randomly sampled linear array

--- sfs/array.py
+++ sfs/array.py
@@ -12,8 +12,55 @@ def linear(N, dx, center=[0, 0, 0], n0=[1, 0, 0]):
     directions = np.tile([1, 0, 0], (N, 1))
     positions, directions = _rotate_array(positions, directions, [1, 0, 0], n0)
     positions += center
+    weights = dx * np.ones(N)
+    return positions, directions, weights
 
-    return positions, directions
+
+def linear_nonuniform(N, dx1, dx2, center=[0, 0, 0], n0=[1, 0, 0]):
+    """Linear secondary source distribution with non-uniform
+       secondary source density."""
+
+    # first segment
+    x00, n00, a00 = linear(N//3, dx2, center=[0, -N//6*(dx1+dx2), 0])
+    positions = x00
+    directions = n00
+    weights = a00
+    # second segment
+    x00, n00, a00 = linear(N//3, dx1)
+    positions = np.concatenate((positions, x00))
+    directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
+    # third segment
+    x00, n00, a00 = linear(N//3, dx2, center=[0, N//6*(dx1+dx2), 0])
+    positions = np.concatenate((positions, x00))
+    directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
+    # shift and rotate array
+    positions, directions = _rotate_array(positions, directions, [1, 0, 0], n0)
+    positions += center
+
+    return positions, directions, weights
+
+
+def linear_random(N, dy1, dy2, center=[0, 0, 0], n0=[1, 0, 0]):
+    """ Randomly sampled linear array."""
+    # vector of uniformly distributed random distances between dy2 > dy1
+    dist = dy1 + (dy2-dy1)*np.random.rand(N-1)
+    # positions of secondary sources
+    positions = np.zeros((N, 3))
+    for m in np.arange(1, N):
+        positions[m, 1] = positions[m-1, 1] + dist[m-1]
+    # weights of secondary sources
+    weights = _weights_linear(positions)
+    # directions of scondary sources
+    directions = np.tile([1, 0, 0], (N, 1))
+    # shift array to center
+    positions[:, 1] -= positions[-1, 1] / 2
+    # shift and rotate array
+    positions, directions = _rotate_array(positions, directions, [1, 0, 0], n0)
+    positions += center
+
+    return positions, directions, weights
 
 
 def circular(N, R, center=[0, 0, 0]):
@@ -26,31 +73,37 @@ def circular(N, R, center=[0, 0, 0]):
     directions = np.zeros_like(positions)
     directions[:, 0] = np.cos(alpha + np.pi)
     directions[:, 1] = np.sin(alpha + np.pi)
-    return positions, directions
+    weights = R * np.ones(N)
+    return positions, directions, weights
 
 
 def rectangular(Nx, dx, Ny, dy, center=[0, 0, 0], n0=None):
     """Rectangular secondary source distribution."""
 
     # left array
-    x00, n00 = linear(Ny, dy)
+    x00, n00, a00 = linear(Ny, dy)
     positions = x00
     directions = n00
+    weights = a00
     # upper array
-    x00, n00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
-                      n0=[0, -1, 0])
+    x00, n00, a00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
+                           n0=[0, -1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # right array
-    x00, n00 = linear(Ny, dy, center=[x00[-1, 0] + dx/2, 0, 0], n0=[-1, 0, 0])
+    x00, n00, a00 = linear(Ny, dy, center=[x00[-1, 0] + dx/2, 0, 0],
+                           n0=[-1, 0, 0])
     x00 = np.flipud(x00)
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # lower array
-    x00, n00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
-                      n0=[0, 1, 0])
+    x00, n00, a00 = linear(Nx, dx, center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
+                           n0=[0, 1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # shift array to center
     positions -= np.asarray([Nx/2 * dx, 0, 0])
     # rotate array
@@ -60,7 +113,7 @@ def rectangular(Nx, dx, Ny, dy, center=[0, 0, 0], n0=None):
     # shift array to desired position
     positions += np.asarray(center)
 
-    return positions, directions
+    return positions, directions, weights
 
 
 def planar(Ny, dy, Nz, dz, center=[0, 0, 0], n0=None):
@@ -76,6 +129,7 @@ def planar(Ny, dy, Nz, dz, center=[0, 0, 0], n0=None):
     # remove first element from initialization
     positions = np.delete(positions, 0, axis=0)
     directions = np.delete(directions, 0, axis=0)
+    weights = dy * dz * np.ones(Ny*Nz)
     # rotate array
     if n0 is not None:
         positions, directions = _rotate_array(positions, directions,
@@ -83,42 +137,50 @@ def planar(Ny, dy, Nz, dz, center=[0, 0, 0], n0=None):
     # shift array to desired position
     positions += np.asarray(center)
 
-    return positions, directions
+    return positions, directions, weights
 
 
 def cube(Nx, dx, Ny, dy, Nz, dz, center=[0, 0, 0], n0=None):
     """Cube shaped secondary source distribtion."""
     center = np.squeeze(np.asarray(center, dtype=np.float64))
     # left array
-    x00, n00 = planar(Ny, dy, Nz, dz)
+    x00, n00, a00 = planar(Ny, dy, Nz, dz)
     positions = x00
     directions = n00
+    weights = a00
     # upper array
-    x00, n00 = planar(Nx, dx, Nz, dz, center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
-                      n0=[0, -1, 0])
+    x00, n00, a00 = planar(Nx, dx, Nz, dz,
+                           center=[Nx/2 * dx, x00[-1, 1] + dy/2, 0],
+                           n0=[0, -1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # right array
-    x00, n00 = planar(Ny, dy, Nz, dz, center=[x00[-1, 0] + dx/2, 0, 0],
-                      n0=[-1, 0, 0])
+    x00, n00, a00 = planar(Ny, dy, Nz, dz, center=[x00[-1, 0] + dx/2, 0, 0],
+                           n0=[-1, 0, 0])
     x00 = np.flipud(x00)
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # lower array
-    x00, n00 = planar(Nx, dx, Nz, dz, center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
-                      n0=[0, 1, 0])
+    x00, n00, a00 = planar(Nx, dx, Nz, dz,
+                           center=[Nx/2 * dx, x00[-1, 1] - dy/2, 0],
+                           n0=[0, 1, 0])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # bottom array
-    x00, n00 = planar(Nx, dx, Ny, dy, center=[Nx/2 * dx, 0, -Nz/2 * dz],
-                      n0=[0, 0, 1])
+    x00, n00, a00 = planar(Nx, dx, Ny, dy, center=[Nx/2 * dx, 0, -Nz/2 * dz],
+                           n0=[0, 0, 1])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # top array
-    x00, n00 = planar(Nx, dx, Ny, dy, center=[Nx/2 * dx, 0, Nz/2 * dz],
-                      n0=[0, 0, -1])
+    x00, n00, a00 = planar(Nx, dx, Ny, dy, center=[Nx/2 * dx, 0, Nz/2 * dz],
+                           n0=[0, 0, -1])
     positions = np.concatenate((positions, x00))
     directions = np.concatenate((directions, n00))
+    weights = np.concatenate((weights, a00))
     # shift array to center
     positions -= np.asarray([Nx/2 * dx, 0, 0])
     # rotate array
@@ -128,7 +190,7 @@ def cube(Nx, dx, Ny, dy, Nz, dz, center=[0, 0, 0], n0=None):
     # shift array to desired position
     positions += np.asarray(center)
 
-    return positions, directions
+    return positions, directions, weights
 
 
 def _rotate_array(x0, n0, n1, n2):
@@ -137,3 +199,16 @@ def _rotate_array(x0, n0, n1, n2):
     x0 = np.inner(x0, R)
     n0 = np.inner(n0, R)
     return x0, n0
+
+
+def _weights_linear(positions):
+    """Calculate loudspeaker weights for linear array."""
+    N = len(positions)
+    weights = np.zeros(N)
+    dy = np.diff(positions[:, 1])
+    weights[0] = dy[0]
+    for m in np.arange(1, N - 1):
+        weights[m] = 0.5 * (dy[m-1] + dy[m])
+    weights[-1] =  dy[-1]
+
+    return weights
--- simple_SFS.py
+++ simple_SFS.py
@@ -10,10 +10,10 @@ import sfs
 
 # parameters
 dx = 0.1  # secondary source distance
-N = 20  # number of secondary sources
-pw_angle = 45  # traveling direction of plane wave
+N = 30  # number of secondary sources
+pw_angle = 0  # traveling direction of plane wave
 xs = [2, 2, 0]  # position of virtual source
-f = 1000  # frequency
+f = 500  # frequency
 
 
 # angular frequency
@@ -23,8 +23,8 @@ omega = 2 * np.pi * f
 npw = sfs.util.normal(np.radians(pw_angle), np.radians(90))
 
 # spatial grid
-x = np.arange(-2, 2, 0.02)
-y = np.arange(-2, 2, 0.02)
+x = np.arange(-.1, 2, 0.02)
+y = np.arange(-3, 3, 0.02)
 
 
 # --------------------------------------------------------------------------------
@@ -32,11 +32,13 @@ y = np.arange(-2, 2, 0.02)
 # --------------------------------------------------------------------------------
 
 # get secondary source positions
-#x0,n0 = sfs.array.linear(N, dx)
-#x0, n0 = sfs.array.circular(N, 1)
-#x0, n0 = sfs.array.rectangular(2*N, dx, N, dx, n0=sfs.util.normal(0*np.pi/4, np.pi/2))
-#x0, n0 = sfs.array.planar(N, dx, N, dx, n0=sfs.util.normal(np.radians(0),np.radians(180)))
-x0, n0 = sfs.array.cube(N, dx, N, dx, N, dx, n0=sfs.util.normal(0, np.pi/2))
+#x0, n0, a0 = sfs.array.linear(N, dx)
+#x0, n0, a0 = sfs.array.circular(N, 1)
+#x0, n0, a0 = sfs.array.rectangular(2*N, dx, N, dx, n0=sfs.util.normal(0*np.pi/4, np.pi/2))
+#x0, n0, a0 = sfs.array.planar(N, dx, N, dx, n0=sfs.util.normal(np.radians(0),np.radians(180)))
+#x0, n0, a0 = sfs.array.cube(N, dx, N, dx, N, dx, n0=sfs.util.normal(0, np.pi/2))
+#x0, n0, a0 = sfs.array.linear_nonuniform(N, dx, 2*dx)
+x0, n0, a0 = sfs.array.linear_random(N, 0.5*dx, 2*dx)
 
 # get driving function
 #d = sfs.mono.drivingfunction.delay_3d_plane(omega, x0, n0, npw)
@@ -61,17 +63,17 @@ twin = sfs.tapering.none(a)
 #twin = sfs.tapering.kaiser(a)
 
 # compute synthesized sound field
-p = sfs.mono.synthesized.generic(omega, x0, d * twin, x, y, 0,
+p = sfs.mono.synthesized.generic(omega, x0, d * twin * a0 , x, y, 0,
                                  source=sfs.mono.source.point)
 
 
 # plot synthesized sound field
 plt.figure(figsize=(15, 15))
-sfs.plot.soundfield(p, x, y, [0, 0, 0])
+sfs.plot.soundfield(p, x, y, [1, 0, 0])
 sfs.plot.loudspeaker_2d(x0, n0, twin)
 plt.grid()
 plt.savefig('soundfield.png')
 
 
-sfs.plot.loudspeaker_3d(x0, n0, twin)
-plt.savefig('loudspeakers.png')
+#sfs.plot.loudspeaker_3d(x0, n0, twin)
+#plt.savefig('loudspeakers.png')
