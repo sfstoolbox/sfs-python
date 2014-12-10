6fef3dc221afdae8a5a594b756c717c9a2ec5532
Sascha Spors <Sascha.Spors@uni-rostock.de>
2014-12-10 15:06:09 +0100

    Origin for plane wave source

--- sfs/mono/source.py
+++ sfs/mono/source.py
@@ -26,11 +26,11 @@ def line(omega, x0, x, y, z=0, c=None):
     return np.squeeze(special.hankel2(0, k * r))
 
 
-def plane(omega, n0, x, y, z=0, c=None):
+def plane(omega, x0, n0, x, y, z=0, c=None):
     """Plane wave"""
     # G(x,w) = e^(-i w/c n x)
     k = util.wavenumber(omega, c)
-    xx, yy, zz = np.meshgrid(x, y, z, sparse=True)
+    xx, yy, zz = np.meshgrid(x - x0[0], y - x0[1], z - x0[2], sparse=True)
     n0 = np.asarray(n0)
     return np.squeeze(
         np.exp(-1j * k * np.inner(np.array([xx, yy, zz], dtype=object), n0)))
