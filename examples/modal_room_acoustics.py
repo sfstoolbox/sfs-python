25f9c714295e6c52d4d7fc0c12e96dd3d887caa2
Sascha Spors <Sascha.Spors@uni-rostock.de>
2015-06-04 10:03:51 +0200

    Minor changes and documentation

--- examples/modal_room_acoustics.py
+++ examples/modal_room_acoustics.py
@@ -1,3 +1,8 @@
+"""
+  This example illustrates the use of the modal room model.
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import sfs
@@ -9,17 +14,18 @@ n0 = [1, 0, 0]  # normal vector of source (only for compatibilty)
 N = 20  # maximum order of modes
 #N = [1, 0, 0]  # room mode to compute
 
-fresponse = 1  # freqeuency response or sound field?
+fresponse = True  # freqeuency response or sound field?
 
-# compute frequency response
-if fresponse is 1:
+# compute and plot frequency response at one point
+if fresponse:
     f = np.linspace(20, 200, 180)  # frequency
     omega = 2 * np.pi * f  # angular frequency
     grid = sfs.util.xyz_grid(1, 1, 1.80, spacing=1)
 
     p = []
     for om in omega:
-        p.append(sfs.mono.source.point_modal(om, x0, n0, grid, L, N=N, deltan=deltan))
+        p.append(sfs.mono.source.point_modal(om, x0, n0, grid, L,
+                                             N=N, deltan=deltan))
 
     p = np.asarray(p)
 
@@ -29,12 +35,13 @@ if fresponse is 1:
     plt.grid()
 
 
-# compute sound field
-if fresponse is 0:
+# compute and plot sound field for one frequency
+if not fresponse:
     f = 500  # frequency
     omega = 2 * np.pi * f  # angular frequency
     grid = sfs.util.xyz_grid([0, L[0]], [0, L[1]], L[2], spacing=.1)
 
     p = sfs.mono.source.point_modal(omega, x0, n0, grid, L, N=N, deltan=deltan)
 
-    sfs.plot.soundfield(p, grid, xnorm=[3, 3, 0], colorbar=False, vmax=1.5, vmin=-1.5)
+    sfs.plot.soundfield(p, grid, xnorm=[2, 3, 0], colorbar=False,
+                        vmax=2, vmin=-2)
