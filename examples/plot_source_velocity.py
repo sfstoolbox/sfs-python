adfe588e3f0c78f3851abf4766a277d9185a6730
Sascha Spors <Sascha.Spors@uni-rostock.de>
2015-10-14 16:21:14 +0200

    Combined plot of pressure distribution and particle displacement

--- examples/plot_source_velocity.py
+++ examples/plot_source_velocity.py
@@ -7,8 +7,8 @@ import sfs
 # simulation parameters
 pw_angle = 45  # traveling direction of plane wave
 f = 300  # frequency
-amplitude = 70000
-t = 0.002
+amplitude = 90000
+t = 0.000
 
 # angular frequency
 omega = 2 * np.pi * f
@@ -16,35 +16,35 @@ omega = 2 * np.pi * f
 npw = sfs.util.direction_vector(np.radians(pw_angle), np.radians(90))
 
 
+# sfs grid object
 #grid = sfs.util.xyz_grid([-2, 2], [-2, 2], 0, spacing=0.5)
 
+# lineary spaced grid
 x = np.linspace(-2, 2, num=50)
 y = np.linspace(-2, 2, num=50)
 grid = sfs.util.asarray_of_arrays( np.meshgrid(x, y) + [0])
 
-#x = np.random.uniform(-2, 2, 5000)
-#y = np.random.uniform(-2, 2, 5000)
+# random grid
+#x = np.random.uniform(-2, 2, 4000)
+#y = np.random.uniform(-2, 2, 4000)
 #grid = sfs.util.asarray_of_arrays( [x, y] + [0])
 
-#grid = sfs.util.xyz_grid(1, 1, 0, spacing=0.2)
-
+# compute velocity and pressure field
 vx, vy, vz = sfs.mono.source.plane_velocity(omega, 0, npw, grid)
+p = sfs.mono.source.plane(omega, 0, npw, grid)
 
-# visualization
+# compute and plot displacement
 grid = sfs.util.asarray_of_arrays(grid[:-1])
 
 #gridx, gridy = grid[:2]
 #grid = sfs.util.asarray_of_arrays([gridx[:2], gridy[:2]])
 
-
-
-
-
 v = sfs.util.asarray_of_arrays([vx, vy])
-
 v = sfs.mono.synthesized.shiftphase(v, omega*t)
-d = grid + amplitude * sfs.util.displacement(v, omega)
+X = grid + amplitude * sfs.util.displacement(v, omega)
 
-plt.figure(figsize=(10, 10))
-sfs.plot.particles(d)
-plt.axis('tight')
+fig, ax = plt.subplots(figsize=(10, 10))
+
+sfs.plot.soundfield(p, grid, ax=ax)
+sfs.plot.particles(X, ax=ax, c=u'k', alpha=.7)
+plt.axis('off')
