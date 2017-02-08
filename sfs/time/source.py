e1a88fda87547df10ec122254933db719334eaa2
Sascha Spors <Sascha.Spors@uni-rostock.de>
2017-02-08 13:44:19 +0100

    Updated documentation

--- sfs/time/source.py
+++ sfs/time/source.py
@@ -64,7 +64,7 @@ def point(xs, signal, t, grid, fs=None, c=None, interpolator_kind='linear'):
 
     if interpolator_kind is 'sinc':
         p = _sinc_interp(signal, np.arange(len(signal)),
-                         np.array((t - g_time) * fs), fs)
+                         np.array((t - g_time) * fs))
     else:
         interpolator = interp1d(np.arange(len(signal)), signal,
                                 kind=interpolator_kind, bounds_error=False,
@@ -75,21 +75,29 @@ def point(xs, signal, t, grid, fs=None, c=None, interpolator_kind='linear'):
     return p * g_amplitude
 
 
-def _sinc_interp(x, s, u, fs):
+def _sinc_interp(x, s, u):
     """
-    Interpolates x, sampled at "s" instants
-    Output y is sampled at "u" instants ("u" for "upsampled")
+    Ideal sinc interpolation of a signal
+    adapted from https://gist.github.com/endolith/1297227
 
-    from Matlab:
-    http://phaseportrait.blogspot.com/2008/06/sinc-interpolation-in-matlab.html        
+    Parameters
+    ----------
+    x : (N,) array_like
+        Signal to be interpolated.
+    s : (N,) array_like
+        Sampling instants of signal.
+    u : (N,) array_like
+        Sampling instants after interpolation.
+
+    Returns
+    -------
+    numpy.ndarray
+        Interpolated signal
     """
 
-    #if len(x) != len(s):
-    #    raise Exception, 'x and s must be the same length'
-
-    # Find the period
+    # sampling period
     T = s[1] - s[0]
-
+    # perform sinc interpolation
     sincM = np.tile(u, (len(s), 1)) - np.tile(s[:, np.newaxis], (1, len(u)))
     y = np.dot(x, np.sinc(sincM/T))
 
