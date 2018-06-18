a9eb961388a04973787d73bff7c15968c14cf3d3
Jeremy Aweda <awedajay@gmail.com>
2018-06-18 23:19:34 +0200

    Tests for sph2cart & cart2sph

---
+++ tests/test_util.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+import sfs
+
+cart_sph_data = [
+    ((1, 1, 1), (np.pi/4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, 1, 1), (np.arctan2(1, -1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((1, -1, 1), (-np.pi/4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, -1, 1), (np.arctan2(-1, -1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((1, 1, -1), (np.pi/4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, 1, -1), (np.arctan2(1, -1), np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((1, -1, -1), (-np.pi/4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, -1, -1), (np.arctan2(-1, -1), np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+]
+
+
+@pytest.mark.parametrize('coord, polar', cart_sph_data)
+def testcart2sph(coord, polar):
+    x, y, z = coord
+    a = sfs.util.cart2sph(x, y, z)
+    assert_allclose(a, polar)
+
+
+@pytest.mark.parametrize('coord, polar', cart_sph_data)
+def testsph2cart(coord, polar):
+    alpha, beta, r = polar
+    b = sfs.util.sph2cart(alpha, beta, r)
+    assert_allclose(b, coord)
