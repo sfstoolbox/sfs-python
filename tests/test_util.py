f1cbe7e1c5bd8a657e0b158d195c28cc7b66d469
Jeremy Aweda <awedajay@gmail.com>
2018-07-01 17:01:33 +0200

    pytest for Sph2Cart & Cart2Sph

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,25 +3,27 @@ from numpy.testing import assert_allclose
 import pytest
 import sfs
 
-test_data = [
-    ((1, 1, 1), (np.pi / 4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
-    ((-1,1,1), (np.arctan2(1,-1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
-    ((1,-1,1), (-np.pi / 4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
-    ((-1,-1,1), (np.arctan2(-1,-1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
-    ((1,1,-1), (np.pi / 4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
-    ((-1,1,-1), (np.arctan2(1,-1), np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
-    ((1,-1,-1), (-np.pi / 4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
-    ((-1,-1,-1), (np.arctan2(-1,-1), np.arccos(-1/np.sqrt(3)), np.sqrt(3)))
+cart_sph_data = [
+    ((1, 1, 1), (np.pi/4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, 1, 1), (np.arctan2(1, -1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((1, -1, 1), (-np.pi/4, np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, -1, 1), (np.arctan2(-1, -1), np.arccos(1/np.sqrt(3)), np.sqrt(3))),
+    ((1, 1, -1), (np.pi/4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, 1, -1), (np.arctan2(1, -1), np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((1, -1, -1), (-np.pi/4, np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
+    ((-1, -1, -1), (np.arctan2(-1, -1), np.arccos(-1/np.sqrt(3)), np.sqrt(3))),
 ]
 
-@pytest.mark.parametrize('coord, polar', test_data)
+
+@pytest.mark.parametrize('coord, polar', cart_sph_data)
 def testcart2sph(coord, polar):
     x, y, z = coord
     a = sfs.util.cart2sph(x, y, z)
-    assert_allclose(a, polar, atol=1e-10)
+    assert_allclose(a, polar, rtol=0, atol=1e-10)
 
-@pytest.mark.parametrize('coord, polar', test_data)
+
+@pytest.mark.parametrize('coord, polar', cart_sph_data)
 def testsph2cart(coord, polar):
     alpha, beta, r = polar
-    b = sfs.util.sph2cart(alpha, beta, r )
-    assert_allclose(b, coord, atol=1e-10)
+    b = sfs.util.sph2cart(alpha, beta, r)
+    assert_allclose(b, coord, rtol=0, atol=1e-10)
